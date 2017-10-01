9e9d590d7e70fc6f3578836ba856ffe7da8ca265
Roberto Carlos Callisaya Mamani <calyr_software@hotmail.com>
2017-10-01 13:34:11 -0400

    Button Back

--- app/src/main/java/ucb/bo/edu/menuprincipal/ServiciosActivity.java
+++ app/src/main/java/ucb/bo/edu/menuprincipal/ServiciosActivity.java
@@ -9,5 +9,8 @@ public class ServiciosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_servicios);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
     }
 }
