4a44c482ae77153cb1b393d848ee09e68f735552
Roberto Carlos Callisaya Mamani <calyr_software@hotmail.com>
2017-10-01 12:45:26 -0400

    Adiccion de propiedad onClick

--- app/src/main/java/ucb/bo/edu/menuprincipal/MenuPrincipalActivity.java
+++ app/src/main/java/ucb/bo/edu/menuprincipal/MenuPrincipalActivity.java
@@ -2,12 +2,26 @@ package ucb.bo.edu.menuprincipal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
+
+    private TextView txtSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+        this.txtSelected = (TextView) findViewById(R.id.txtSelected);
+    }
+
+
+    public void selectButton(View view) {
+        if (view.getId() == R.id.btnEventos) {
+            this.txtSelected.setText("Eventos");
+        } else if (view.getId() == R.id.btnPortafolio) {
+            this.txtSelected.setText("Portafolio");
+        }
     }
 }
